74a6352f4c0ce2f3836b3ea4012a4d2806c1d8ef
zieglerr <roman-ziegler@gmx.net>
2023-10-04 17:52:27 +0200

    Second commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="Test">
-      <h1>I am a Demo</h1>
+      <h1>I am a Demoooo</h1>
       <h2 className='Oi'>Getting to know Netify</h2>
     </div>
   );
